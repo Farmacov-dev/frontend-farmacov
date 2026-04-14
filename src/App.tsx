59987e42240f91fda582f075340a87253abcf75d
DurandalAGB <a01667100@tec.mx>
2026-04-14 13:35:08 -0600

    angel: implementar componente textarea

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
 import { useState } from "react"
-import SearchInput from "./components/primary/SearchInput/SearchInput"
+import TextAreaField from "./components/primary/TextAreaField/TextAreaField"
 
 export default function App() {
-  const [query, setQuery] = useState("")
-  
+  const [texto, setTexto] = useState("")  
   return (
     <>
-      <SearchInput
-        value={query}
-        onChange={setQuery}
-        placeholder="Busqueda"
-      />
+    //sin limite
+      <TextAreaField
+  label="Observaciones y análisis"
+  placeholder="Describe tus hallazgos..."
+  value={texto}
+  onChange={(e) => setTexto(e.target.value)}
+/>
+
+// limite de caracteres con contador
+<TextAreaField
+  label="Observaciones y análisis"
+  placeholder="Describe tus hallazgos..."
+  value={texto}
+  onChange={(e) => setTexto(e.target.value)}
+  maxLength={500}
+  showCount
+/>
+
+// Con error
+<TextAreaField
+  label="Observaciones y análisis"
+  error
+  value={texto}
+  onChange={(e) => setTexto(e.target.value)}
+/>
     </>
   )
 }
