c5d3bf5c2b5dc039b7070c707a0cb6f427edc022
Emmanuel López Paredes <emmanuel@192.168.1.67>
2026-04-24 12:28:59 -0600

    Add: Catalog Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/Login";
+import AppRouter from "./router/AppRouter";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={
-          <div className="min-h-screen flex items-center justify-center">
-            <p className="font-inter text-[24px] font-medium text-dark">
-              Dashboard — próximamente
-            </p>
-          </div>
-        } />
-      </Routes>
-    </BrowserRouter>
+    <AppRouter />
   );
 }
