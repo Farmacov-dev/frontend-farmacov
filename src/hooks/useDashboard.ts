f09f4ab1d37e59caf03241a6751b6314b3104541
JoseMiguel <l>
2026-05-11 19:44:53 -0600

    Implementación de una nueva grafica costos por vacuna en Dashboard

--- src/hooks/useDashboard.ts
+++ src/hooks/useDashboard.ts
@@ -3,6 +3,7 @@ import { useQuery } from '@tanstack/react-query'
 import { getKpis } from '../services/dashboard/getKpis'
 import { getTopSintomas } from '../services/dashboard/getTopSintomas'
 import { getEfectividad } from '../services/dashboard/getEfectividad'
+import { getCostosVacuna } from '../services/dashboard/getCostosVacuna'
 import { CACHE_24H, CACHE_15MIN } from '../config/queryClient'
 
 export const useKpis = () => {
@@ -27,6 +28,15 @@ export const useEfectividad = () => {
   return useQuery({
     queryKey: ['efectividad'],
     queryFn: getEfectividad,
+    staleTime: CACHE_15MIN,
+    gcTime: CACHE_15MIN,
+  })
+}
+
+export const useCostosVacuna = () => {
+  return useQuery({
+    queryKey: ['costos-vacuna'],
+    queryFn: getCostosVacuna,
     staleTime: CACHE_15MIN,
     gcTime: CACHE_15MIN,
   })
